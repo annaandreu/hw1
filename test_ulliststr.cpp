af4ba097d4f340a5b28eb320de9f7ed423597394
Anna Andreu <aandreu@usc.edu>
2023-01-27 04:05:54 +0000

    part 2 completed

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -4,10 +4,61 @@
 #include <sstream>
 
 #include "ulliststr.h"
+using namespace std;
 
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+ULListStr dat;
+
+
+
+/*
+dat.push_back("sup");
+dat.push_back("er");
+dat.push_back("cal");
+dat.push_back("i");
+dat.push_back("frag");
+dat.push_back("il");
+dat.push_back("istic");
+dat.push_back("espi");
+dat.push_back("alo");
+dat.push_back("doc");
+dat.push_back("ious");
+
+dat.pop_back();
+
+cout << dat.size();*/
+
+//dat.push_back("1"); 
+//dat.push_front("8");
+
+/*dat.push_back("7");
+dat.push_back("4");
+dat.push_back("6");
+dat.push_back("2");
+dat.push_back("5");
+dat.push_back("6");
+dat.push_back("0");
+dat.push_back("2");
+dat.push_back("1"); */
+//dat.push_front("8");
+//dat.pop_back();
+//dat.pop_front();
+
+/*size_t traverse = 0;
+cout << "list: ";
+while(traverse < dat.size()){ 
+  cout << dat.get(traverse) << " ";
+  traverse++;
+    }
+
+cout << endl;
+cout << "front: " << dat.front(); 
+cout << endl;
+cout << "back: " << dat. back();
+cout << endl;
+*/
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,168 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val) { 
+// CASE 1 : Array is empty
+  if (empty()){ 
+    Item* Node1 = new Item();
+    Node1 -> val[Node1 -> first] = val; // first spot 
+    Node1 -> last= Node1 -> last +1 ; // one after last used index 
+    
+    head_ = Node1;
+    tail_ = Node1; 
+  }
+  // CASE 2: No room to push_back - create new node 
+  else if ((tail_ -> last) > (ARRSIZE - 1) ){
+    Item* Node2 = new Item(); // allocaate new var 
+    tail_ -> next = Node2; // point to next item
+    
+    Node2 -> prev = tail_; // before node 2 comes tail (node1)
+    //Node2 -> next = nullptr;
+    tail_ = Node2; // Node2 becomes new tail_ 
+
+	//assign first and last pointers
+    tail_ -> first = 0;
+		tail_ -> last = (tail_ -> first) +1; // one after 
+		
+		tail_ -> val[tail_ -> first] = val; 
+    //tail_ -> val[tail_ -> last] = val; // val = last slot of tail_ node
+		//tail_ -> last = (tail_ -> last) + 1; // update last index (1 after new val) 
+  }
+  //CASE 3: Room to push_back
+  else{
+    tail_ -> val[tail_ -> last] = val; // val = last in tail node 
+		tail_ -> last = (tail_ -> last) + 1; // update last index (1 after new val) 
+    //tail_ -> prev = head_; 
+  }
+size_++; 
+}
+
+void ULListStr::pop_back(){
+  // array is empty- nothing 
+	if (empty()){ 
+    return; 
+  }
+  // one item in array
+  else if (tail_ -> last == tail_ -> first +1){ 
+      if(tail_-> prev == nullptr) { // ONLY one node 
+        delete tail_;  
+        head_ = nullptr;
+        tail_ = nullptr;
+        size_ =0;
+        return;
+      }
+      else{ // MORE nodes
+      tail_ = tail_-> prev; 
+      delete tail_ -> next;
+      tail_-> next = nullptr;
+      }
+    }
+   //NORMAL - remove number from first index in array
+  else {
+  tail_ -> last = (tail_ -> last) - 1;
+  }
+
+  size_ --;  
+}
+  
+
+void ULListStr::push_front(const std::string& val){
+  // CASE 1: array is empty  
+  if (empty()){ 
+    Item* Node_1 = new Item();
+    Node_1 -> first = ARRSIZE -1; // slot items at back of array
+    Node_1 -> last = ARRSIZE;
+
+    Node_1 -> val[Node_1 -> first] = val; // first spot 
+    //Node_1 -> last= (Node_1 -> first) + 1; 
+    
+    head_ = Node_1;
+    tail_ = Node_1;
+  }
+  // CASE 2: no room to push_front - create new node 
+  else if (head_ -> first == 0){
+    // allocate new item
+    Item* Node_0 = new Item(); 
+    
+    // set pointers
+    head_ -> prev = Node_0; // prev is Node_0 (new item)
+    Node_0 -> next = head_; // next is head_ (old item)
+    head_ = Node_0;  // make front the new head_
+
+    head_ -> first = ARRSIZE -1; // first value = last slot 
+    head_ -> last = ARRSIZE; // last is index before 
+    
+    head_ -> val[head_ -> first] = val; // set val to first 
+  }
+  // CASE 3- room to push_front
+  else{ 
+    head_ -> first = head_ -> first - 1; // update first location 
+    head_ -> val[head_ -> first] = val; // add new val to first 
+    //head_ -> next = tail_;
+    
+  }
+
+size_++; 
+ }
+
+void ULListStr::pop_front(){
+  if (head_ != NULL) { 
+    if (!(empty())) { // list is not empty - MOST cases 
+      head_-> first= head_-> first + 1;  // just move index 
+      size_--;
+    }
+    
+    // if only one item left
+    if (head_-> first == head_-> last) {
+      //ONLY node left 
+      if (head_ == tail_) {
+        Item* temp = head_;
+        head_ = nullptr;
+        tail_ = nullptr;
+        delete temp;
+      }
+      //more than one node - more items in list 
+      else {
+        Item* temp = head_;
+        head_ = head_->next;
+        delete temp;
+      }
+    }
+  }
+ }
+
+  std::string const & ULListStr::back() const{
+    return tail_ -> val[(tail_ -> last) -1]; // return last value 
+  }
+
+  std::string const & ULListStr::front() const{
+    return head_ -> val[(head_ -> first)]; // return first value 
+  }
+
+  std::string* ULListStr::getValAtLoc(size_t loc) const{
+    Item* position = head_; 
+		size_t loc_index = 0;
+
+// while in range 
+		while (loc >= 0 && loc < size_){
+			// traverse all items in node 
+			for (size_t i = (position -> first); i < (position -> last); i++) {
+			if (loc == loc_index){ // if we reach the location's index 
+				return &(position -> val[i]);
+			}
+			loc_index++; 
+		}
+		// if not in first node - move to next node
+		position = position -> next;
+		}
+    if (loc > size_){
+      return NULL;
+    }
+    if (size_ == 0){
+      return NULL;
+    }
+	
+  }
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
@@ -63,3 +225,6 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+
+// ListInsertBack.OneItemAdd
