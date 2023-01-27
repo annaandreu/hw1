278609f95b68ee308aeade7c05d250ffe5abba49
Anna Andreu <aandreu@usc.edu>
2023-01-27 04:04:43 +0000

    part 1 complete

--- split.cpp
+++ split.cpp
@@ -11,13 +11,36 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <iostream>
+using namespace std;
 
 /* Add a prototype for a helper function here if you need */
 
-void split(Node*& in, Node*& odds, Node*& evens)
+void split(Node*& in, Node*& odds, Node*& evens) // *& can change value and address 
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  Node* position = in; // create copy of in pointer to track current position and traverse list
+
+  if (position == nullptr ){ // list is empty
+    odds = nullptr;
+    evens = nullptr;
+    return; 
+  }
+
+  else if ((position -> value) % 2 == 0){ // if even 
+    evens = position; // assign pos to even
+    //cout << "even" << evens -> value << endl; 
+    position = position -> next; // move down in position
+    split(position, odds, evens -> next); 
+    //recursively call again but moving down in 1 & odd 1 */
+  }
+  
+  else { // if odd 
+    odds = position;  
+    //cout << "odds" << odds -> value << endl;
+    position = position -> next;
+    split(position, odds -> next, evens); 
+  }
+  in = nullptr;
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,45 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+//#include <iostream>
+//using namespace std;
 
 int main(int argc, char* argv[])
 {
+  return 0;
+/*
+  // initialize your in nodes 
+  Node* five = new Node(5, nullptr);
+  Node* four = new Node(4, five);
+  Node* three = new Node(3, four);
+  Node* two = new Node(2, three);
+  Node* one = new Node(1, two);
 
+// You may assume that odds and evens are both NULL when split is called 
+  Node* odds = nullptr; 
+  Node* evens = nullptr;
+
+split(one, odds, evens);
+
+//print lists 
+  while (evens != nullptr){
+    cout << evens-> value << " ";
+    evens= evens -> next;
+  }
+  cout << endl; 
+
+  while (odds != nullptr){
+    cout << odds-> value << " ";
+    odds= odds -> next;
+  }
+  cout << endl; 
+
+
+// delete memory
+  delete five; 
+  delete four; 
+  delete three;
+  delete two;
+  delete one;
+  */
 }
